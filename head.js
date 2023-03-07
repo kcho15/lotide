003a02abb8974c5984dd8e644e4118f3049e8c80
Kevin Cho <kevcho604@gmail.com>
2023-03-06 17:47:34 -0800

    Function that returns the first element of an array input

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✨✨✨ Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`💀💀💀 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(2,2); 
+
+const head = function(input) {
+  let result = input[0];
+  return result; 
+}
+
+assertEqual(head([]), 5);  
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
