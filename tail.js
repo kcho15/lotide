a87c4e6afee269ff7ec682fdd1c5da1883815592
Kevin Cho <kevcho604@gmail.com>
2023-03-06 18:32:53 -0800

    Function that returns the elements of an array minus the very first element

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✨✨✨ Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`💀💀💀 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(2,2);
+
+const tail = function(input) {
+  if (input.length === 0) {
+    return [];
+  }
+  return input.slice(1);
+};
+
+console.log(tail([1,2,3]));
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
