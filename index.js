3cc35c8be91bd9a89bf90cccc0c5e2b26ad31bdc
Kevin Cho <kevcho604@gmail.com>
2023-03-13 20:12:07 -0700

    Updated index list of modules exported as objects

---
+++ index.js
@@ -0,0 +1,25 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const findKey = require('./findKey');
+const map = require('./map');
+const countLetters = require('./countLetters')
+const without = require('./without')
+const takeUntil = require('./takeUntil')
+const eqArrays = require('./eqArrays')
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  eqArrays: eqArrays,
+  findKey: findKey,
+  map: map, 
+  countLetters: countLetters,
+  without: without, 
+  takeUntil: takeUntil,
+  eqArrays: eqArrays
+
+}; 
